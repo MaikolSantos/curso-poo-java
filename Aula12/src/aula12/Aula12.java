13c2ad680dfc8e3452155ee861f3521dcc3046ad
Maikol Lourençon dos Santos <maikol.lourencon@gmail.com>
2020-11-18 23:48:09 -0300

    add exercise
    12

---
+++ Aula12/src/aula12/Aula12.java
@@ -0,0 +1,32 @@
+package aula12;
+
+public class Aula12 {
+
+    public static void main(String[] args) {
+        //Animal n = new Animal();
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+        Canguru cang = new Canguru();
+        Cachorro cach = new Cachorro();
+        Cobra cob = new Cobra();
+        Tartaruga t = new Tartaruga();
+        GoldFish g = new GoldFish();
+        Arara ara = new Arara();
+        
+        m.alimentar();
+        r.locomover();
+        p.emitirSom();
+        a.alimentar();
+        cang.locomover();
+        t.locomover();
+        cach.emitirSom();
+        
+        
+        
+        
+        
+    }
+    
+}
